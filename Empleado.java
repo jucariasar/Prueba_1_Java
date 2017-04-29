18bc366d18cdc456c752a3d606bca97df17ed1d4
jucariasar <jucariasar@unal.edu.co>
2017-04-29 01:47:47 -0500

    Probando GitHub en Linux

---
+++ Empleado.java
@@ -0,0 +1,24 @@
+
+public class Empleado
+{
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Empleado(){
+		this.id = 1;
+		this.nombre = "Carlos";
+		this.apellido = "Barrientos";	
+	
+	}
+
+	
+	public static void main(String[] args)
+	{
+		Empleado e1 = new Empleado();
+		
+	}	
+	
+
+}
